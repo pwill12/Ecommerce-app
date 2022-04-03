79b8bb7d90d023c63a0be28b6d51d56e51483644
Princewill <princesolo919@gmail.com>
2022-04-03 09:55:04 +0100

    added Productlist page

---
+++ src/Pages/Productlist.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+export default function ProductList() {
+    
+}
